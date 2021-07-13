bd00a3bf9488bc5cba37834051495b6d3e999531
thanhhoa214 <hoa.nguyen@nng.bz>
2021-07-13 11:54:50 +0700

    first commit

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', event => {
+    console.log('Service worker has been installed.', event);
+});
+self.addEventListener('activate', event => {
+    console.log('Service worker has been activated.', event);
+});
+self.addEventListener('fetch', event => {
+    console.log('Service worker has been fetched.', event);
+});
